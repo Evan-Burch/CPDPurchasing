fece5ea961fb1fec9d518f8f49718dc220f1af24
Garrett Hayes <garrettbridgeshayes@gmail.com>
2024-06-20 19:15:21 -0500

    fixed po call

--- server/index.js
+++ server/index.js
@@ -139,7 +139,7 @@ app.post("/fillPOTable", async (req, res) => {
 
 		console.log("Filling the PO Table");
 
-		POTable = await dbConnection.query("SELECT * FROM tblPurchaseOrder LIMIT 10;");
+		POTable = await dbConnection.query("SELECT * FROM tblPurchaseOrder");
 
 		for (let i = 0; i < POTable.length; i++) {
 			const VendorQuery = await dbConnection.query("SELECT VendorName FROM tblVendor WHERE VendorID=?;", [POTable[i].VendorID]);
