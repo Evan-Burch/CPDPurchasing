187e1c04eb2bb4783c33439212ccbdd35cae9e46
Evan Burch <edburch42@tntech.edu>
2024-09-16 20:50:09 -0500

    `Primary`

--- server/index.js
+++ server/index.js
@@ -174,13 +174,11 @@ app.post("/addVendor", async (req, res) => {
 
 		// Figure out what the next auto-increment ID is for tblVendorContact so we can use it for tblVendor
 		const intVendorContactID = await dbConnection.query("SELECT MAX(ID) AS maxID FROM tblVendorContact;");
-		console.log("intVendorContactID2: ", intVendorContactID[0].maxID);
-		console.log("intVendorContactID2: ", intVendorContactID[0].maxID + 1);
 		const insertVendorResult = await dbConnection.query("INSERT INTO tblVendor (VendorName, Website, Status, VendorContactID) VALUES (?, ?, 1, ?);", [strVendorName, strLink, intVendorContactID[0].maxID + 1]);
 		
 		// Get the ID of the newly inserted vendor to use for tblVendorContact
 		const intVendorID = insertVendorResult.insertId;
-		await dbConnection.query("INSERT INTO tblVendorContact (ID, VendorID, Name, Primary, DateAdded, CreatedBy, Status) VALUES (?, ?, ?, 1, NOW(), ?, 1);", [intVendorContactID[0].maxID + 1, intVendorID, strVendorName, intCreatedBy]);
+		await dbConnection.query("INSERT INTO tblVendorContact (ID, VendorID, Name, `Primary`, DateAdded, CreatedBy, Status) VALUES (?, ?, ?, 1, NOW(), ?, 1);", [intVendorContactID[0].maxID + 1, intVendorID, strVendorName, intCreatedBy]);
 
     	res.json({"message": "Success.", "status": 200});
 	} finally {
