e99edb0711d3b2dfb221da99aea5d2a939c23cb0
Haley Burnell <hales8torr@gmail.com>
2024-10-08 23:17:03 -0500

    add backend for bars

--- server/methods/home-page.js
+++ server/methods/home-page.js
@@ -53,6 +53,54 @@ router.post("/fillDonutChart", async(req,res) => {
 	}
 });
 
+router.post("/fillBarChart", async(req,res) => {
+	const dbConnection = await db_pool.getConnection();
+	const uuidSessionToken = clean(req.headers.uuidsessiontoken);
+	
+	try {
+		var userID = await getUserIDBySessionToken(uuidSessionToken);
+		if (userID == -1) {
+			return res.status(400).json({"message": "You must be logged in to do that"});
+		}
+
+		console.log("Getting data for bar chart");
+
+		// we want the most recent year
+		var report = {Budget: BudgetInfo[BudgetInfo.length - 1].Amount}; 
+		
+		// last two digits of year plus 42- prefix
+		const strLike = "42-" + BudgetInfo[BudgetInfo.length - 1].FiscalYear % 100 + "%"; 
+
+		let TotalsByAccountQuery = `
+			WITH this_year as (select * FROM tblPurchaseOrderItem where PurchaseOrderID like "${strLike}"),
+			account_chars as (select AccountID, CAST(AccountID AS CHAR) AS account_char, Price from this_year),
+			these_categories as (SELECT
+				CASE
+				WHEN account_char LIKE '42110%' THEN '42110'
+				WHEN account_char LIKE '42130%' THEN '42130'
+				WHEN account_char LIKE '42140%' THEN '42140'
+				WHEN account_char LIKE '42150%' THEN '42150'
+				ELSE 'other'
+				END as account_category,
+				Price
+				FROM account_chars)
+			select account_category, SUM(Price) as current_total
+			from these_categories group by account_category;
+		`;
+
+		accountTotals = await dbConnection.query(TotalsByAccountQuery);
+
+		if(accountTotals.length == 0){
+			return res.status(500).json({"message": "error getting accounts"});
+		}
+
+		res.status(200).json({"message": "Success.", "accountTotals": accountTotals});
+
+	} finally {
+		await dbConnection.close();
+	}
+});
+
 router.post("/getActivity", async (req, res) => {
 	const dbConnection = await db_pool.getConnection();
 	const uuidSessionToken = clean(req.body.uuidSessionToken);
